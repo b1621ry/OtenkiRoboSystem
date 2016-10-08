55ff6645a0d6f9040b67e39897ec06375566ae44
b1621ry <b1621ry@aiit.ac.jp>
2016-10-08 10:09:56 +0900

    いらないファイルの削除

--- OtenkiGet/command.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ("whoami" コマンドをパスに有するシステム上で)
-// 実行中のphp/httpdプロセスを所有するユーザーの名前を出力
-echo exec('ls -l');
-echo exec('rm log.txt');
-echo exec('java -jar WISclient.jar');
-echo exec('telnet localhost 9001');
-?>
