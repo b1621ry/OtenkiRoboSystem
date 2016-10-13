805c191c5cb0bf7cccafcc48cdacc97b6dbee124
b1621ry <b1621ry@aiit.ac.jp>
2016-10-13 16:13:21 +0900

    晴れの場合のエラー修正

--- OtenkiProcess/otenkiresult.php
+++ OtenkiProcess/otenkiresult.php
@@ -9,9 +9,9 @@ $result =file_get_contents($fileName);
 //天気の判別
 if(preg_match('/くもり/',$result) || preg_match('/雨/',$result) || preg_match('/雪/',$result) )
 {
-    echo exec('say te ru te ru ki doooo');
-}else if(preg_match('晴れ',$result)){
-    echo exec('say ama go i ki doooo');
+    echo exec('say te      ru      te     ru     ki    do');
+}else if(preg_match('/晴れ/',$result)){
+    echo exec('say ama go e ki do');
 }
 
 ?>
